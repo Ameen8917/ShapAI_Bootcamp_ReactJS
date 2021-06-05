d502a241b3c9d74ab98a79654e3c4d3acbaa89e0
Ameen8917 <70534240+Ameen8917@users.noreply.github.com>
2021-06-04 23:47:24 -0700

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="card-note">
+      <h1>Modern ReactJS</h1>
+      <p>
+        React JS is basically a JavaScript library built and maintained by
+        Facebook. it has taken the front-end development space by storm.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
